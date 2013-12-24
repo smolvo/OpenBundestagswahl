11eeb28839df0c6738b2f2cbba29f4afaaec1b13
Nick Vlasoff <nickvlasoff@googlemail.com>
2013-12-24 13:56:04 +0000

    Mandatsklasse hinzugefügt

---
+++ Implementierung/Mandatsrechner/src/Model/Mandat.java
@@ -0,0 +1,42 @@
+package Model;
+/**
+ * Enum-Klasse für Art des Mandats, dass ein 
+ * @author Nick
+ *
+ */
+public enum Mandat {
+	/**
+	 * Durch Zweitstimmen zugeteiltes Mandat
+	 */
+	MANDAT("Mandat"),
+	/**
+	 * Durch die meisten Erststimmen zugeteiltes Mandat
+	 */
+	DIREKMANDAT("Direktmadat"), 
+	/**
+	 * Mandat mit mehr Direkmandate als durch Zweitstimmen zugeteilte Mandate
+	 */
+	UEBERHANGMADAT("Überhangmandat"),
+	/**
+	 * Durch Überhangmandate zugeteilte Mandate zum Ausgleich
+	 */
+	AUSGLEICHSMANDAT("Ausgleichsmandat"),
+	/**
+	 * Kein Mandat
+	 */
+	KEINMANDAT("Kein Mandat");
+	/**
+	 * Beinhaltet die Bezeichnung des Mandats
+	 */
+	private String mandat;
+	/**
+	 * Kontsruktor der die Bezeichnung setzt
+	 * @param mandat die Bezeichnung des Mandats
+	 */
+	Mandat (String mandat){ this.mandat = mandat;}
+	/**
+	 * Gibt die Bezeichnung des Mandats zurück
+	 * @return die Bezeichnung des Mandats
+	 */
+	public String toString(){return this.mandat;}
+}
