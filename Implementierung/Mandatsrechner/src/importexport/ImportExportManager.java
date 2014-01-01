7ccf5821e9744bb5de9af0dbf2f0eacb25ab3720
Enes Ördek <genesis@proxer.de>
2014-01-01 23:20:24 +0000

    Javadoc Kommentare & Checkstyle (Tests)

--- Implementierung/Mandatsrechner/src/importexport/ImportExportManager.java
+++ Implementierung/Mandatsrechner/src/importexport/ImportExportManager.java
@@ -4,14 +4,32 @@ import java.io.File;
 
 import model.Bundestagswahl;
 
+/**
+ * Der ImportExportManager ist zustaendig fuer das Erstellen
+ * von "Bundestagswahl"-Objekten.
+ * @author Enes Oerdek
+ *
+ */
 public class ImportExportManager {
+	/**
+	 * Ein Array, dass alle vorhandenen Crawler-Algorithmen
+	 * beinhaltet.
+	 */
 	private Crawler crawler[];
 	
+	/**
+	 * Hier koennen weitere Algorithmen ergaenzt werden.
+	 */
 	public ImportExportManager(){
 		crawler = new Crawler[1];
 		crawler[0] = new Crawler2013();
 	}
 
+	/**
+	 * Importiert eine Datei.
+	 * @param csvDatei Datei die importiert werden soll.
+	 * @return das Ergebnis-Objekt.
+	 */
 	public Bundestagswahl importieren(File csvDatei){
 		return null;
 	}
