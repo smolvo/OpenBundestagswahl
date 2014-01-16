52f13be8abbb541b04c5657a53be217efabdf137
Simon Schürg <simon.schuerg@gmail.com>
2014-01-16 12:38:03 +0000

    Wahlgenerator erweitert

--- Implementierung/Mandatsrechner/src/wahlgenerator/AbstrakterWahlgenerator.java
+++ Implementierung/Mandatsrechner/src/wahlgenerator/AbstrakterWahlgenerator.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package wahlgenerator;
 
 import java.util.List;
@@ -8,43 +5,46 @@ import java.util.List;
 import model.Bundestagswahl;
 
 /** 
- * 
  * Mit dieser Klasse können Bundestagswahl- Objekte anhand gegebener
- * Stimmanteile auf Bundesebene generiert werden
- *
- * @author Manuel
- *
+ * Stimmanteile auf Bundesebene generiert werden.
  */
 public abstract class AbstrakterWahlgenerator {
 
-	/** Die Basiswahl, mit relevanten Strukturdaten */
+	/** Die Basiswahl mit relevanten Strukturdaten. */
 	private Bundestagswahl basisWahl;
 	
-	/** Die vom Benutzer gegebenen Stimmanteile */
+	/** Die Liste der Stimmanteile pro Partei. */
 	private List<Stimmanteile> stimmanteile;
 	
-	/** Die Anzahl der Erststimmen aller Wahlberechtigten */
+	/** Die Anzahl der Erststimmen aller Wahlberechtigten. */
 	private Integer anzahlErststimmen;
 	
-	/*TODO
-	/bin mir nicht ganz sicher, was mit anzahlZweistimmen bzw
-	 anzahlErststimmen gemeint ist
-	*/
-	/** Die Anzahl der Zweitstimmen aller Wahlberechtigten */
+	/** Die Anzahl der Zweitstimmen aller Wahlberechtigten. */
 	private Integer anzahlZweitstimmen;
 	
+	
 	/**
-	 * Der Konstruktor dieser Klasse
-	 * @param basisWahl
-	 * @param stimmanteile
+	 * Parametrisierter Konstruktor.
+	 * 
+	 * @param basisWahl Die Basiswahl mit relevanten Strukturdaten.
+	 * @param stimmanteile Die Liste der Stimmanteile pro Partei.
 	 */
-	public AbstrakterWahlgenerator(Bundestagswahl basisWahl, List<Stimmanteile> stimmanteile)  {
+	public AbstrakterWahlgenerator(Bundestagswahl basisWahl, List<Stimmanteile> stimmanteile) {
 		this.basisWahl = basisWahl;
 		this.stimmanteile = stimmanteile;
 		
 		berechneGesamtanzahlStimmen();
-	}
+	}	
 	
+	/**
+	 * Erzeugt eine neue Bundestagswahl auf der Grundlage der basisWahl und füllt diese
+	 *
+	 * @return generierte Bundestagswahl
+	 */
+	public Bundestagswahl erzeugeBTW() {
+		//TODO
+		return null;
+	}
 	
 	/**
 	 * Berechnet anzahlZweitstimmen und anzahlErststimmen
@@ -55,20 +55,90 @@ public abstract class AbstrakterWahlgenerator {
 		//TODO
 	}
 	
-	
-	/**
-	 * Erzeugt eine neue Bundestagswahl auf der Grundlage der basisWahl und füllt diese
-	 *
-	 * @return erstlle Bundestagswahl
-	 */
-	public Bundestagswahl erzeugeBTW() {
-		//TODO
-		return null;
-	}
-	
 	/**
 	 * Diese Methode verteilt alle Erst- und Zweitstimmen auf die Wahlkreise
 	 * der Bundestagswahl.
 	 */
 	public abstract void verteileStimmen();
+
+	/**
+	 * Gibt die Basiswahl mit relevanten Strukturdaten zurück.
+	 * @return Die Basiswahl mit relevanten Strukturdaten.
+	 */
+	public Bundestagswahl getBasisWahl() {
+		return basisWahl;
+	}
+
+	/**
+	 * Setzt die Basiswahl mit relevanten Strukturdaten.
+	 * @param basisWahl Die Basiswahl mit relevanten Strukturdaten.
+	 * @throws IllegalArgumentException wenn der Parameter basisWahl null ist.
+	 */
+	public void setBasisWahl(Bundestagswahl basisWahl) throws IllegalArgumentException {
+		if (basisWahl == null) {
+			throw new IllegalArgumentException("Der Parameter \"basisWahl\" ist null!");
+		}
+		this.basisWahl = basisWahl;
+	}
+
+	/**
+	 * Gibt die Liste der Stimmanteile pro Partei zurück.
+	 * @return die Liste der Stimmanteile pro Partei.
+	 */
+	public List<Stimmanteile> getStimmanteile() {
+		return stimmanteile;
+	}
+
+	/**
+	 * Setzt die Liste der Stimmanteile pro Partei.
+	 * @param stimmanteile Die Liste der Stimmanteile pro Partei.
+	 * @throws IllegalArgumentException Wenn der Parameter stimmanteile leer oder null ist.
+	 */
+	public void setStimmanteile(List<Stimmanteile> stimmanteile) throws IllegalArgumentException {
+		if (stimmanteile == null || stimmanteile.isEmpty()) {
+			throw new IllegalArgumentException("Der Parameter \"stimmanteile\" ist leer oder null!");
+		}
+		this.stimmanteile = stimmanteile;
+	}
+
+	/**
+	 * Gibt die Gesamtanzahl der Erststimmen zurück.
+	 * @return die Gesamtzahl der Erststimmen
+	 */
+	public Integer getAnzahlErststimmen() {
+		return anzahlErststimmen;
+	}
+	
+	/**
+	 * Setzt die Gesamtzahl der Erststimmen.
+	 * @param anzahlErststimmen Die Gesamtzahl der Erststimmen.
+	 * @throws IllegalArgumentException Wenn der Parameter anzahlErststimmen ist negativ ist.
+	 */
+	public void setAnzahlErststimmen(Integer anzahlErststimmen) throws IllegalArgumentException {
+		if (anzahlErststimmen < 0) {
+			throw new IllegalArgumentException("Der Parameter \"anzahlErststimmen\" ist negativ!");
+		}
+		this.anzahlErststimmen = anzahlErststimmen;
+	}
+
+	/**
+	 * Gibt die Gesamtanzahl der Zweitstimmen zurück.
+	 * @return die Gesamtzahl der Zweitstimmen
+	 */
+	public Integer getAnzahlZweitstimmen() {
+		return anzahlZweitstimmen;
+	}
+
+	/**
+	 * Setzt die Gesamtzahl der Zweitstimmen.
+	 * @param anzahlZweitstimmen Die Gesamtzahl der Zweitstimmen.
+	 * @throws IllegalArgumentException Wenn der Parameter anzahlZweitstimmen ist negativ ist.
+	 */
+	public void setAnzahlZweitstimmen(Integer anzahlZweitstimmen) throws IllegalArgumentException {
+		if (anzahlZweitstimmen < 0) {
+			throw new IllegalArgumentException("Der Parameter \"anzahlErststimmen\" ist negativ!");
+		}
+		this.anzahlZweitstimmen = anzahlZweitstimmen;
+	}
+	
 }
