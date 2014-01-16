87cc73d0cd1e19dc74531fa0fc1ad4d8dcad509e
Enes Ördek <genesis@proxer.de>
2014-01-16 18:19:44 +0000

    Chronik (Container) fertiggestellt.

--- Implementierung/Mandatsrechner/src/chronik/Chronik.java
+++ Implementierung/Mandatsrechner/src/chronik/Chronik.java
@@ -1,13 +1,53 @@
 package chronik;
 
+import java.util.Stack;
+
 import model.Stimme;
 
+/**
+ * Die Chronik setzt Stimmen, die in der GUI verändert
+ * wurden zurück. Es können höchstens so viele Stimmen 
+ * zurückgesetzt werden, wie in maxStimmen.
+ * @author 13genesis37
+ *
+ */
 public class Chronik {
-	public void sichereStimme(Stimme s){
+	
+	/**
+	 * Anzahl der maximalen Stimmen, die zurückgesetzt
+	 * werden können.
+	 */
+	final private int maxStimmen = 5; 
+	
+	/**
+	 * Ein Stack mit den letzten Stimmen
+	 */
+	Stack<Stimme> stimmen = new Stack<Stimme>();
+	/**
+	 * Sichert eine Stimme. Wird bei jeder Veränderung
+	 * einer Stimme aufgerufen. Die Operation entspricht
+	 * dem push Befehl eines Stacks.
+	 * @param s die Veränderte Stimme.
+	 */
+	public void sichereStimme(Stimme s) {
 		
+		if (this.stimmen.size() > maxStimmen) {
+			this.stimmen.remove(0);
+		}
+		
+		this.stimmen.push(s);
 	}
 	
-	public Stimme restauriereStimme(){
-		return null;
+	/**
+	 * Restauriert die zuletzt hinzugefügte Stimme.
+	 * Entspricht dem pull Befehl eines Stacks.
+	 * @return die zuletzt hinzugefügte Stimme.
+	 */
+	public Stimme restauriereStimme() {
+		Stimme s = null;
+		if (this.stimmen.size() > 0) {
+			s = this.stimmen.pop();
+		}
+		return s;
 	}
 }
