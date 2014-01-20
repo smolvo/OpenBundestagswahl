c1a68accabea2ccb78d59fd8f014b5249e932bb5
Anton Mehlmann <mehlton@web.de>
2014-01-20 09:40:40 +0000

    Steuerung

---
+++ Implementierung/Mandatsrechner/src/steuerung/Steuerung.java
@@ -0,0 +1,23 @@
+package steuerung;
+
+import model.Stimme;
+
+public class Steuerung {
+
+	
+	public Steuerung() {
+		
+	}
+	
+	public void importieren() {
+		
+	}
+	
+	public void exportieren() {
+		
+	}
+	
+	public void aktualisiereDaten(Stimme stimme, int anzahl) {
+		
+	}
+}
