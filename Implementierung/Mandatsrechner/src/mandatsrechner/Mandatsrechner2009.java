0ce12935a2ab49677b219987fdcf6771746549ee
Nick Vlasoff <nickvlasoff@googlemail.com>
2014-01-20 19:36:07 +0000

    Mandatsrechner 2009 erweitert

--- Implementierung/Mandatsrechner/src/mandatsrechner/Mandatsrechner2009.java
+++ Implementierung/Mandatsrechner/src/mandatsrechner/Mandatsrechner2009.java
@@ -1,73 +1,98 @@
 package mandatsrechner;
 
 import model.*;
-import java.util.LinkedList; //Später rausmachen
 
-public class Mandatsrechner2009 extends Mandatsrechner{
-	
+import java.util.LinkedList; //Spaeter rausmachen
+
+public class Mandatsrechner2009 extends Mandatsrechner {
+
 	private static Mandatsrechner2009 instance;
 	private final int minSitze = 598;
 	private float zuteilungsdivisor = 0;
 
 	private int sperrklauselAnzahl;
 	private final int minDirektmandate = 3;
-	
-	
-	
+	/** relevante Dateien**/
+	private LinkedList<Partei> relevanteParteien = new LinkedList<Partei>();
 	public static Mandatsrechner2009 getInstance() {
-		if(Mandatsrechner2009.instance==null){
+		if (Mandatsrechner2009.instance == null) {
 			Mandatsrechner2009.instance = new Mandatsrechner2009();
 		}
 		return Mandatsrechner2009.instance;
 	}
 	
-	public Bundestagswahl berechneAlles(Bundestagswahl bw){
-		
-		
+	/**
+	 * Setzt die neue Parteiliste
+	 * @param parteiliste die neue Parteiliste
+	 */
+	public void setRelevanteParteien(LinkedList<Partei> parteiliste){
+		if(parteiliste == null){
+			throw new IllegalArgumentException("Parteiliste ist leer");
+		}
+		this.relevanteParteien = parteiliste;
+	}
+	
+	/**
+	 * Gibt die Liste mit relevanten Parteien
+	 * @return die Liste mit den relevanten Parteien
+	 */
+	public LinkedList<Partei> getRelevanteParteien(){
+		return this.relevanteParteien;
+	}
+	
+	public Bundestagswahl berechneAlles(Bundestagswahl bw) {
+
 		// Initialisierung:
 		this.sperrklauselAnzahl = bw.getDeutschland().getZweitstimmeGesamt() / 20;
 		bw.getSitzverteilung().setAbgeordnete(new LinkedList<Kandidat>());
-		
-		//**Sitze für jedes Bundesland mithilge des zuteilungsdivisor berechnen
+
+		// **Sitze fuer jedes Bundesland mithilge des zuteilungsdivisor
+		// berechnen
 		zuteilungsdivisor = 0;
 		boolean isCorrect = false;
 		int sitzanzahl;
 		zuteilungsdivisor = bw.getDeutschland().getEinwohneranzahl() / minSitze;
-		while(!isCorrect){
+		while (!isCorrect) {
 			sitzanzahl = 0;
-			for(Bundesland bl : bw.getDeutschland().getBundeslaender()){
-				//TODO Nach erster Nachkommastelle
-				sitzanzahl += Math.round(bl.getEinwohnerzahl()/zuteilungsdivisor);
+			for (Bundesland bl : bw.getDeutschland().getBundeslaender()) {
+				// TODO Nach erster Nachkommastelle
+				sitzanzahl += Math.round(bl.getEinwohnerzahl()
+						/ zuteilungsdivisor);
 			}
-			if(sitzanzahl == minSitze){
+			if (sitzanzahl == minSitze) {
 				isCorrect = true;
-			}else if(sitzanzahl < minSitze){
+			} else if (sitzanzahl < minSitze) {
 				zuteilungsdivisor -= 0.1;
-			}else{
-				//sitzanzahl > minSitze
+			} else {
+				// sitzanzahl > minSitze
 				zuteilungsdivisor += 0.1;
 			}
 		}
-		//**Ende
-		
-		if(super.debug){
-			System.out.println("Zuteilungsdivisor: "+zuteilungsdivisor);
-			for(Bundesland bl : bw.getDeutschland().getBundeslaender()){
-				System.out.println(bl.getName()+": "+Math.round(bl.getEinwohnerzahl()/zuteilungsdivisor));
+		// **Ende
+
+		if (super.debug) {
+			System.out.println("Zuteilungsdivisor: " + zuteilungsdivisor);
+			for (Bundesland bl : bw.getDeutschland().getBundeslaender()) {
+				System.out
+						.println(bl.getName()
+								+ ": "
+								+ Math.round(bl.getEinwohnerzahl()
+										/ zuteilungsdivisor));
 			}
 		}
-		
-		//**Direkmandate bestimmen
-		for(Bundesland bl : bw.getDeutschland().getBundeslaender()){
-			for(Wahlkreis wk : bl.getWahlkreise()){
+
+		// **Direkmandate bestimmen
+		for (Bundesland bl : bw.getDeutschland().getBundeslaender()) {
+			for (Wahlkreis wk : bl.getWahlkreise()) {
 				int max = 0;
 				Kandidat gewinner = null;
-				for(Erststimme erst : wk.getErststimmen()){
-					//TODO parallelität!
-					//TODO Kandidaten mit gleicher Erststimmenanzahl
-					
-					if(max < erst.getAnzahl()){
-						//Kandidaten Mandat zuweisen und als Wahlkreissieger in den Wahlkreis eintragen
+				for (Erststimme erst : wk.getErststimmen()) {
+					// TODO parallelitï¿½t!
+					// TODO Kandidaten mit gleicher Erststimmenanzahl
+
+					if (max < erst.getAnzahl()) {
+						// Kandidaten Mandat zuweisen und als Wahlkreissieger in
+						// den Wahlkreis eintragen
 						gewinner = erst.getKandidat();
 						max = erst.getAnzahl();
 					}
@@ -75,114 +100,135 @@ public class Mandatsrechner2009 extends Mandatsrechner{
 				gewinner.setMandat(Mandat.DIREKMANDAT);
 				wk.setWahlkreisSieger(gewinner);
 				bw.getSitzverteilung().addAbgeordnete(gewinner);
-			
+
 			}
-		}	
-		//**Ende
-		//**relevanten Parteien bestimmen
-			/*
-			 * Hier findet die Überprüfung der Sperrklausel, doch wo sind alle Parteien?
-			 * Ich bräuchte eine Liste von allen Parteien die überprüft werden müssen
-			 * die die Bedingungen erfüllen (Sperrklausel) kommen in den Bundestag (Flag setzen)
-			 * Als platzhalter erstelle ich vorerst eine Liste alleParteien
-			 */	
-		//Platzhalter 
-		LinkedList<Partei> alleParteien = bw.getParteien(); // Alle Parteien der Bundestagswahl
-		//relevante Parteien
-		LinkedList<Partei> relevanteParteien = new LinkedList<Partei>();
-			
-		for(Partei part : alleParteien){
-				
-			if(part.getZweitstimmeGesamt() >= this.sperrklauselAnzahl || part.getAnzahlDirektmandate() >= this.minDirektmandate) {
-				// Partei im Bundestag falls Anforderungen erfüllt sind.
+		}
+		// **Ende
+		// **relevanten Parteien bestimmen
+		/*
+		 * Hier findet die ï¿½berprï¿½fung der Sperrklausel, doch wo sind alle
+		 * Parteien? Ich brï¿½uchte eine Liste von allen Parteien die ï¿½berprï¿½ft
+		 * werden mï¿½ssen die die Bedingungen erfï¿½llen (Sperrklausel) kommen in
+		 * den Bundestag (Flag setzen) Als platzhalter erstelle ich vorerst eine
+		 * Liste alleParteien
+		 */
+		// Platzhalter
+		LinkedList<Partei> alleParteien = bw.getParteien(); // Alle Parteien der
+															// Bundestagswahl
+		// relevante Parteien
+		
+
+		for (Partei part : alleParteien) {
+
+			if (part.getZweitstimmeGesamt() >= this.sperrklauselAnzahl
+					|| part.getAnzahlDirektmandate() >= this.minDirektmandate) {
+				// Partei im Bundestag falls Anforderungen erfï¿½llt sind.
 				part.setImBundestag(true);
-				//Partei in die Liste hinzufügen
-				if(!relevanteParteien.contains(part)){
+				// Partei in die Liste hinzufï¿½gen
+				if (!relevanteParteien.contains(part)) {
 					relevanteParteien.add(part);
 				}
 			} else {
 				// Ansonsten ist die Partei nicht im Bundestag.
 				part.setImBundestag(false);
 			}
-					
+
 		}
-			
-		if(this.debug){
+
+		if (this.debug) {
 			System.out.println("\nParteien im Bundestag:");
-			for(Partei part : bw.getParteien()){
-				System.out.println(part.getName()+": "+((part.isImBundestag())?"Ja":"Nein"));
+			for (Partei part : bw.getParteien()) {
+				System.out.println(part.getName() + ": "
+						+ ((part.isImBundestag()) ? "Ja" : "Nein"));
 			}
 		}
-		
-		float landesdivisor=0;
-		for(Bundesland bl : bw.getDeutschland().getBundeslaender()){
-			
-			int sitzeBundesland = Math.round(bl.getEinwohnerzahl()/zuteilungsdivisor);
+
+		float landesdivisor = 0;
+		for (Bundesland bl : bw.getDeutschland().getBundeslaender()) {
+
+			int sitzeBundesland = Math.round(bl.getEinwohnerzahl()
+					/ zuteilungsdivisor);
 			landesdivisor = bl.getZweitstimmeGesamt() / sitzeBundesland;
 			isCorrect = false;
-			//System.err.println(sitzeBundesland+" "+landesdivisor);
-			while(!isCorrect){
+			// System.err.println(sitzeBundesland+" "+landesdivisor);
+			while (!isCorrect) {
 				int sitzePartei = 0;
-				
-				for(Partei part : relevanteParteien){
-					//TODO Nach erster Nachkommastelle
-					sitzePartei += Math.round(bl.getZweitstimmenAnzahl(part)/landesdivisor);
+
+				for (Partei part : relevanteParteien) {
+					// TODO Nach erster Nachkommastelle
+					sitzePartei += Math.round(bl.getZweitstimmenAnzahl(part)
+							/ landesdivisor);
 				}
-				//System.out.println(sitzePartei + " " + sitzeBundesland);
-				if(sitzePartei == sitzeBundesland){
+				// System.out.println(sitzePartei + " " + sitzeBundesland);
+				if (sitzePartei == sitzeBundesland) {
 					isCorrect = true;
-				}else if(sitzePartei < sitzeBundesland){
+				} else if (sitzePartei < sitzeBundesland) {
 					landesdivisor -= 0.5;
-				}else{
-					//sitzanzahl > sitzeBundesland
+				} else {
+					// sitzanzahl > sitzeBundesland
 					landesdivisor += 0.5;
 				}
 			}
-			
-			for(Partei part : relevanteParteien){
-				int direktmandate = part.getAnzahlMandate(Mandat.DIREKMANDAT, bl);
-				int mindestSitzanzahl = Math.round(bl.getZweitstimmenAnzahl(part)/landesdivisor);
+
+			for (Partei part : relevanteParteien) {
+				int direktmandate = part.getAnzahlMandate(Mandat.DIREKMANDAT,
+						bl);
+				int mindestSitzanzahl = Math.round(bl
+						.getZweitstimmenAnzahl(part) / landesdivisor);
 				int diffKandidat = mindestSitzanzahl - direktmandate;
-				part.addMindestsitzanzahl(bl, Math.max(direktmandate, mindestSitzanzahl));
-				if(diffKandidat > 0){
-					for(int i = 0 ; i <= diffKandidat ; i++){
-						//Nehme aus der Bundestagswahl die Landesliste der Partei und füge den i-ten Listenkandidaten in die Sitzverteilung hinzu
-						if(bl.getLandesliste(part).getListenkandidaten().size() >= i+1){
-							//TODO Testen
-							bw.getSitzverteilung().addAbgeordnete(bl.getLandesliste(part).getListenkandidaten().get(i));
-						}else{
-							//TODO negatives Stimmengewicht
+				part.addMindestsitzanzahl(bl,
+						Math.max(direktmandate, mindestSitzanzahl));
+				if (diffKandidat > 0) {
+					for (int i = 0; i <= diffKandidat; i++) {
+						// Nehme aus der Bundestagswahl die Landesliste der
+						// Partei und fï¿½ge den i-ten Listenkandidaten in die
+						// Sitzverteilung hinzu
+						if (bl.getLandesliste(part).getListenkandidaten()
+								.size() >= i + 1) {
+							// TODO Testen
+							bw.getSitzverteilung().addAbgeordnete(
+									bl.getLandesliste(part)
+											.getListenkandidaten().get(i));
+							bl.getLandesliste(part).getListenkandidaten()
+									.get(i).setMandat(Mandat.MANDAT);
+						} else {
+							// TODO negatives Stimmengewicht
 						}
 					}
-				}else{
-					//TODO Überhangmandate
-					
+				} else {
+					for(int i = 0 ; i < diffKandidat ; i++){
+						bl.getDirektMandate(part).get(i).setMandat(Mandat.UEBERHANGMADAT);
+					}
 				}
-					
-				
-			
+
 			}
-			if(this.debug){
-				System.out.println("\nLandesdivisor "+bl.getName()+": "+landesdivisor);
-				for(Partei part : relevanteParteien){
-					System.out.println("Sitze "+part.getName()+": "+Math.round(bl.getZweitstimmenAnzahl(part)/landesdivisor));
+			if (this.debug) {
+				System.out.println("\nLandesdivisor " + bl.getName() + ": "
+						+ landesdivisor);
+				for (Partei part : relevanteParteien) {
+					System.out.println("Sitze "
+							+ part.getName()
+							+ ": "
+							+ Math.round(bl.getZweitstimmenAnzahl(part)
+									/ landesdivisor));
 				}
-				
+
 			}
-			
+
 		}
-		
+
 		return bw;
 	}
-	
+
 	@Override
 	public Bundestagswahl berechne(Bundestagswahl bw) {
 		// TODO Sitzanzahl als konstante setzen
-		
+
 		float bundestagsdivisor = bw.getDeutschland().getEinwohneranzahl() / 598;
-		
-		for(Bundesland bl : bw.getDeutschland().getBundeslaender()){
-			// TODO Divisor muss so angepasst werden damit die Anzahl der Sitze stimmen
+
+		for (Bundesland bl : bw.getDeutschland().getBundeslaender()) {
+			// TODO Divisor muss so angepasst werden damit die Anzahl der Sitze
+			// stimmen
 			float sitzeBundesland = bl.getEinwohnerzahl() / bundestagsdivisor;
 			berechne(bl, (int) sitzeBundesland);
 		}
@@ -192,58 +238,57 @@ public class Mandatsrechner2009 extends Mandatsrechner{
 	@Override
 	protected Bundesland berechne(Bundesland bl, int sitzeBundesland) {
 		// TODO Auto-generated method stub
-		
+
 		int zweitstimmenanzahl = 0;
-		for(int i = 0; i < bl.getZweitstimmen().size(); i++){
+		for (int i = 0; i < bl.getZweitstimmen().size(); i++) {
 			zweitstimmenanzahl += bl.getZweitstimmen().get(i).getAnzahl();
 		}
-		//Mindestsitzanzahl in landesliste abspeichern
-		//Landesdivisor muss immer angepasst werden
-		float landesdivisor =  zweitstimmenanzahl / sitzeBundesland;
-		
-		for(Wahlkreis wk : bl.getWahlkreise()){
+		// Mindestsitzanzahl in landesliste abspeichern
+		// Landesdivisor muss immer angepasst werden
+		float landesdivisor = zweitstimmenanzahl / sitzeBundesland;
+
+		for (Wahlkreis wk : bl.getWahlkreise()) {
 			berechne(wk);
 		}
-		
+
 		// TODO Mehr Direktmandaten als mindesitze?
-		
+
 		// TODO Sitze in landesliste setzen
-		
-	
-		
-		// TODO Restliche Sitze mit Mandate füllen
-		
+
+		// TODO Restliche Sitze mit Mandate fï¿½llen
+
 		return bl;
 	}
 
 	@Override
 	protected Wahlkreis berechne(Wahlkreis wk) {
 		// TODO Auto-generated method stub
-		if(wk == null){
+		if (wk == null) {
 			throw new IllegalArgumentException("Wahlkreis ist leer");
 		}
 		int max = 0;
 		Kandidat gewinner = null;
-		for(Erststimme erst : wk.getErststimmen()){
-			//TODO parallelität!
-			//TODO Kandidaten mit gleicher Erststimmenanzahl
-			
-			if(max < erst.getAnzahl()){
-				//Kandidaten Mandat zuweisen und als Wahlkreissieger in den Wahlkreis eintragen
+		for (Erststimme erst : wk.getErststimmen()) {
+			// TODO parallelitï¿½t!
+			// TODO Kandidaten mit gleicher Erststimmenanzahl
+
+			if (max < erst.getAnzahl()) {
+				// Kandidaten Mandat zuweisen und als Wahlkreissieger in den
+				// Wahlkreis eintragen
 				gewinner = erst.getKandidat();
 				max = erst.getAnzahl();
 			}
 		}
 		gewinner.setMandat(Mandat.DIREKMANDAT);
 		wk.setWahlkreisSieger(gewinner);
-		//TODO Eintrag im Bericht für den Direktmandat setzen
+		// TODO Eintrag im Bericht fï¿½r den Direktmandat setzen
 		return wk;
 	}
 
 	@Override
 	protected void erstelleBericht(String zeile) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
--- Implementierung/Mandatsrechner/src/model/Bundesland.java
+++ Implementierung/Mandatsrechner/src/model/Bundesland.java
@@ -6,71 +6,86 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Klasse die die Bundesländer repräsentiert.
- * Unterklasse von Gebiet.
+ * Klasse die die Bundeslï¿½nder reprï¿½sentiert. Unterklasse von Gebiet.
  */
-public class Bundesland extends Gebiet implements Serializable, Comparable<Bundesland> {
+public class Bundesland extends Gebiet implements Serializable,
+		Comparable<Bundesland> {
 
-	/** Automatisch generierte serialVersionUID die für das De-/Serialisieren verwendet wird. */
+	/**
+	 * Automatisch generierte serialVersionUID die fï¿½r das De-/Serialisieren
+	 * verwendet wird.
+	 */
 	private static final long serialVersionUID = 1614716230171638779L;
 
 	/** Einwohnerzahl des Bundeslandes. */
 	private int einwohnerzahl;
-	
+
 	/** Farbe des Bundeslandes */
 	private Color farbe;
-	
+
 	/** Liste mit den Wahlkreisen im Bundesland */
 	private LinkedList<Wahlkreis> wahlkreise = new LinkedList<Wahlkreis>();
-	
+
 	/** Liste mit den vertrettenden Parteien im Bundesland */
 	private LinkedList<Partei> parteien = new LinkedList<Partei>();
-	
+
 	private List<Landesliste> landesliste = new LinkedList<Landesliste>();
-	
+
 	/**
-	 * Parametrisierter Konstruktor für Bundesländer.
-	 * Listen werden seperat hinzugefügt
-	 * @param name Der name des Bundeslandes
-	 * @param einwohnerzahl Die Anzahl der Einwohner
+	 * Parametrisierter Konstruktor fï¿½r Bundeslï¿½nder. Listen werden seperat
+	 * hinzugefï¿½gt
+	 * 
+	 * @param name
+	 *            Der name des Bundeslandes
+	 * @param einwohnerzahl
+	 *            Die Anzahl der Einwohner
 	 */
 	public Bundesland(String name, int einwohnerzahl) {
 		this.setName(name);
 		this.setEinwohnerzahl(einwohnerzahl);
 	}
-	
+
 	/**
-	 * Gibt die Einwohnerzahl zurück
+	 * Gibt die Einwohnerzahl zurï¿½ck
+	 * 
 	 * @return die Einwohnerzahl
 	 */
 	public int getEinwohnerzahl() {
 		return einwohnerzahl;
 	}
-	
+
 	/**
 	 * Setzt die Einwohnerzahl
-	 * @param einwohnerzahl wird neu gesetzt
-	 * @throws IllegalArgumentException wenn die zahl negativ ist
+	 * 
+	 * @param einwohnerzahl
+	 *            wird neu gesetzt
+	 * @throws IllegalArgumentException
+	 *             wenn die zahl negativ ist
 	 */
-	public void setEinwohnerzahl(int einwohnerzahl) throws IllegalArgumentException {
+	public void setEinwohnerzahl(int einwohnerzahl)
+			throws IllegalArgumentException {
 		if (einwohnerzahl < 0) {
 			throw new IllegalArgumentException("Einwohnerzahl ist kleiner 0");
 		}
 		this.einwohnerzahl = einwohnerzahl;
 	}
-	
+
 	/**
-	 * Gibt die Farbe des Bundeslandes zurück
+	 * Gibt die Farbe des Bundeslandes zurï¿½ck
+	 * 
 	 * @return die Farbe
 	 */
 	public Color getFarbe() {
 		return farbe;
 	}
-	
+
 	/**
 	 * Setzt die Farbe des Bundeslandes
-	 * @param farbe des Bundeslandes
-	 * @throws IllegalArgumentException wenn die Farbe leer ist
+	 * 
+	 * @param farbe
+	 *            des Bundeslandes
+	 * @throws IllegalArgumentException
+	 *             wenn die Farbe leer ist
 	 */
 	public void setFarbe(Color farbe) throws IllegalArgumentException {
 		if (farbe == null) {
@@ -78,66 +93,79 @@ public class Bundesland extends Gebiet implements Serializable, Comparable<Bunde
 		}
 		this.farbe = farbe;
 	}
-	
+
 	/**
-	 * Gibt eine Liste mit den Wahlkreisen zurück
+	 * Gibt eine Liste mit den Wahlkreisen zurï¿½ck
+	 * 
 	 * @return die Liste mit Wahlkreisen
 	 */
 	public LinkedList<Wahlkreis> getWahlkreise() {
 		return wahlkreise;
 	}
-	
+
 	/**
 	 * Setzt eine neue Liste mit Wahlkreisen
-	 * @param wahlkreise die neue Liste
-	 * @throws IllegalArgumentException wenn die Liste leer ist
+	 * 
+	 * @param wahlkreise
+	 *            die neue Liste
+	 * @throws IllegalArgumentException
+	 *             wenn die Liste leer ist
 	 */
-	public void setWahlkreise(LinkedList<Wahlkreis> wahlkreise) throws IllegalArgumentException {
+	public void setWahlkreise(LinkedList<Wahlkreis> wahlkreise)
+			throws IllegalArgumentException {
 		if (wahlkreise == null || wahlkreise.isEmpty()) {
 			throw new IllegalArgumentException("Wahlkreisliste ist leer");
 		}
 		this.wahlkreise = wahlkreise;
 	}
-	
+
 	/**
-	 * Gibt die Liste mit Parteien zurück
+	 * Gibt die Liste mit Parteien zurï¿½ck
+	 * 
 	 * @return die Liste mit Parteien
 	 */
 	public LinkedList<Partei> getParteien() {
 		return parteien;
 	}
-	
+
 	/**
 	 * Setzt eine neue Liste mit Parteien
-	 * @param parteien die neue Liste
-	 * @throws IllegalArgumentException wenn die Liste leer ist
+	 * 
+	 * @param parteien
+	 *            die neue Liste
+	 * @throws IllegalArgumentException
+	 *             wenn die Liste leer ist
 	 */
-	public void setParteien(LinkedList<Partei> parteien) throws IllegalArgumentException {
+	public void setParteien(LinkedList<Partei> parteien)
+			throws IllegalArgumentException {
 		if (parteien == null || parteien.isEmpty()) {
 			throw new IllegalArgumentException("Name ist leer");
 		}
 		this.parteien = parteien;
 	}
-	
+
 	/**
-	 * Fügt eine Partei zur Liste hinzu
-	 * @param partei ist der neue Partei
+	 * Fï¿½gt eine Partei zur Liste hinzu
+	 * 
+	 * @param partei
+	 *            ist der neue Partei
 	 */
 	public void addPartei(Partei partei) {
 		if (partei == null) {
-		      throw new IllegalArgumentException("Partei ist leer!");
+			throw new IllegalArgumentException("Partei ist leer!");
 		}
 		this.parteien.add(partei);
 	}
-	
 
 	/**
-	 * Fügt einen Wahlkreis zur Liste hinzu
-	 * @param wahlkreis Der Wahlkreis der hinzugefügt wird
+	 * Fï¿½gt einen Wahlkreis zur Liste hinzu
+	 * 
+	 * @param wahlkreis
+	 *            Der Wahlkreis der hinzugefï¿½gt wird
 	 */
 	public void addWahlkreis(Wahlkreis wahlkreis) {
 		if (wahlkreis == null) {
-		      throw new IllegalArgumentException("Wahlkreis ist leer!");
+			throw new IllegalArgumentException("Wahlkreis ist leer!");
 		}
 		this.wahlkreise.add(wahlkreis);
 	}
@@ -145,46 +173,48 @@ public class Bundesland extends Gebiet implements Serializable, Comparable<Bunde
 	@Override
 	public void setZweitstimmen(LinkedList<Zweitstimme> zweitstimmen) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public List<Erststimme> getErststimmen() {
 		List<Erststimme> erststimmen = new LinkedList<Erststimme>();
-		int[] tempStimmen = new int[this.wahlkreise.get(0).getErststimmen().size()];
+		int[] tempStimmen = new int[this.wahlkreise.get(0).getErststimmen()
+				.size()];
 		for (int i = 0; i < this.wahlkreise.size(); i++) {
-			List<Erststimme> wahlkreisErststimme = wahlkreise.get(i).getErststimmen();
+			List<Erststimme> wahlkreisErststimme = wahlkreise.get(i)
+					.getErststimmen();
 			for (int j = 0; j < wahlkreisErststimme.size(); j++) {
 				tempStimmen[j] += wahlkreisErststimme.get(j).getAnzahl();
 			}
 		}
 		for (int i = 0; i < tempStimmen.length; i++) {
-			erststimmen.add(new Erststimme(
-					tempStimmen[i],
-					this,
-					new Kandidat("Unbekannt", "Unbekannt", 0, Mandat.KEINMANDAT, null)));
+			erststimmen.add(new Erststimme(tempStimmen[i], this, new Kandidat(
+					"Unbekannt", "Unbekannt", 0, Mandat.KEINMANDAT, null)));
 		}
-		
+
 		return erststimmen;
 	}
 
 	@Override
 	public List<Zweitstimme> getZweitstimmen() {
 		List<Zweitstimme> zweitstimmen = new LinkedList<Zweitstimme>();
-		int[] tempZweitstimmen = new int[this.wahlkreise.get(0).getZweitstimmen().size()];
+		int[] tempZweitstimmen = new int[this.wahlkreise.get(0)
+				.getZweitstimmen().size()];
 		for (int i = 0; i < this.wahlkreise.size(); i++) {
-			List<Zweitstimme> wahlkreisZweitstimme = wahlkreise.get(i).getZweitstimmen();
+			List<Zweitstimme> wahlkreisZweitstimme = wahlkreise.get(i)
+					.getZweitstimmen();
 			for (int j = 0; j < wahlkreisZweitstimme.size(); j++) {
 				tempZweitstimmen[j] += wahlkreisZweitstimme.get(j).getAnzahl();
 			}
 		}
 		for (int i = 0; i < tempZweitstimmen.length; i++) {
-			zweitstimmen.add(new Zweitstimme(
-					tempZweitstimmen[i],
-					this,
-					this.wahlkreise.get(0).getZweitstimmen().get(i).getPartei()));
+			zweitstimmen
+					.add(new Zweitstimme(tempZweitstimmen[i], this,
+							this.wahlkreise.get(0).getZweitstimmen().get(i)
+									.getPartei()));
 		}
-		
+
 		return zweitstimmen;
 	}
 
@@ -200,32 +230,40 @@ public class Bundesland extends Gebiet implements Serializable, Comparable<Bunde
 	@Override
 	public void setWahlberechtigte(int wahlberechtigte) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	/**
 	 * Setzt eine neue Liste von Landeslisten.
-	 * @param landesliste Die Landesliste die gesetzt wird.
-	 * @throws IllegalArgumentException wenn die Liste leer ist
+	 * 
+	 * @param landesliste
+	 *            Die Landesliste die gesetzt wird.
+	 * @throws IllegalArgumentException
+	 *             wenn die Liste leer ist
 	 */
-	public void setLandeliste(List<Landesliste> landesliste) throws IllegalArgumentException {
+	public void setLandeliste(List<Landesliste> landesliste)
+			throws IllegalArgumentException {
 		if (landesliste == null || landesliste.isEmpty()) {
 			throw new IllegalArgumentException("landesliste ist leer");
 		}
 		this.landesliste = landesliste;
 	}
-	
+
 	/**
-	 * Gibt die Liste aller Landeslisten dieses Bundeslandes zurück.
+	 * Gibt die Liste aller Landeslisten dieses Bundeslandes zurï¿½ck.
+	 * 
 	 * @return die Landesliste dieses Bundeslandes
 	 */
 	public List<Landesliste> getLandesliste() {
 		return this.landesliste;
 	}
-	
+
 	/**
-	 * Fügt eine Landesliste zu dieser der Liste aller Landeslisten dieses Bundeslandes hinzu.
-	 * @param landesliste Die Landesliste die hinzugefügt wird.
+	 * Fï¿½gt eine Landesliste zu dieser der Liste aller Landeslisten dieses
+	 * Bundeslandes hinzu.
+	 * 
+	 * @param landesliste
+	 *            Die Landesliste die hinzugefï¿½gt wird.
 	 */
 	public void addLandesliste(Landesliste landesliste) {
 		this.landesliste.add(landesliste);
@@ -239,20 +277,40 @@ public class Bundesland extends Gebiet implements Serializable, Comparable<Bunde
 	@Override
 	public int getZweitstimmenAnzahl(Partei partei) {
 		int anzahl = 0;
-		for(Wahlkreis wk : this.getWahlkreise()){
+		for (Wahlkreis wk : this.getWahlkreise()) {
 			anzahl += wk.getZweitstimmenAnzahl(partei);
 		}
 		return anzahl;
 	}
-	
-	public Landesliste getLandesliste(Partei partei){
+
+	public Landesliste getLandesliste(Partei partei) {
 		Landesliste result = null;
-		for(Landesliste land : this.getLandesliste()){
-			if(land.getPartei().equals(partei)){
+		for (Landesliste land : this.getLandesliste()) {
+			if (land.getPartei().equals(partei)) {
 				result = land;
 			}
 		}
 		return result;
-		
+
+	}
+
+	/**
+	 * Gibt eine Liste mit den Kandidaten die ein Direktmandate
+	 * 
+	 * @param part
+	 *            die Partei der Kandidaten
+	 * @return Liste mit den Direktmandate einer Partei
+	 */
+	public LinkedList<Kandidat> getDirektMandate(Partei part){
+		if(part == null){
+			throw new IllegalArgumentException("Partei ist leer");
+		}
+		LinkedList<Kandidat> direktmandate = new LinkedList<Kandidat>();
+		for (Wahlkreis wk : this.getWahlkreise()){
+			if (wk.getWahlkreisSieger().getPartei() == part){
+				direktmandate.add(wk.getWahlkreisSieger());
+			}
+		}
+		return direktmandate;
 	}
 }
