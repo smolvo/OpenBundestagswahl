20f4cd4eb03fd32a516a707d557eae70b9519371
Philipp Löwer <ph.loewer@hotmail.de>
2014-01-20 17:44:07 +0000

    Funktionalität der Deutschlandkarte geändert. ResizeListener wird noch hinzugefügt um die skalierbarkeit zu gewährleisten.

--- Implementierung/Mandatsrechner/src/gui/ansicht/DeutschlandKarte.java
+++ Implementierung/Mandatsrechner/src/gui/ansicht/DeutschlandKarte.java
@@ -2,6 +2,9 @@ package gui.ansicht;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -20,7 +23,7 @@ import model.Deutschland;
  */
 public class DeutschlandKarte extends JPanel {
 
-	/** alle Bundesländer, nötig für Färbung */
+	/** alle Bundeslaender, nötig für Färbung */
 	private Deutschland land;
 	
 	/**
@@ -31,125 +34,126 @@ public class DeutschlandKarte extends JPanel {
 		this.land = land;
 	}
 	
+	/** wahr wenn bereits bilder importiert wurden, sonst falsch */
+	boolean bilderImportiert = false;
+	
+	/** in diesem array werden die importierten Bilder der Bundeslaender gespeichert */
+	BufferedImage[] bundeslandBilder = new BufferedImage[16];
+	
+	/** in diesem Array werden die Namen der importierten Bundeslaender gespeichert */
+	String[] bundeslandNamen = new String[16];
+	
+	/** in diesem Array werden die Bilder der Bundeslaender nachdem sie eingefärbt wurden gespeichert */
+	BufferedImage[] bundeslandBilderGefaerbt = new BufferedImage[16];
+
+	/** Dieser String gibt den Pfad des Speicherortes der Bundesland-Bilder an */
+	String pfad = "src/gui/resources/Bundeslaender/";
+	
+	/** erstellt ein BufferedImage in der größe 900 * 1024  */
+	BufferedImage grossVersion = new BufferedImage(900, 1024, BufferedImage.TYPE_INT_ARGB);
+	
+	/**  */
+	Image skaliert = null;
+	
+	/** skaliert das Bild nachdem die Bilder der Bundeslaender hinzugefügt 
+	 * wurden entsprechend der aktuellen größe des JPanels 
+	 */
+	public void skaliere() {
+	skaliert = grossVersion.getScaledInstance(this.getWidth(), this.getHeight(), Image.SCALE_SMOOTH);
+	}
+	
 	/**
 	 * @param g  
-	 * 
+	 * führt alle noetigen Aktionen aus um die eingefaerbte Deutschlandkare zu erstellen
 	 */
 	public void paintComponent(Graphics g) {
-		LinkedList<Bundesland> bundeslaender = land.getBundeslaender();
-		
-		for (Bundesland bl: bundeslaender) {
-			if (bl.getName().equals("Baden-Württemberg")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Baden-Württemberg.png");
-				BufferedImage badenWuerttemberg = faerbeLand(bwfarbe, landimg);
-				g.drawImage(badenWuerttemberg, 30, 229, this);
-			} else if (bl.getName().equals("Bayern")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Bayern.png");
-				BufferedImage bayern = faerbeLand(bwfarbe, landimg);
-				g.drawImage(bayern, 79, 202, this);
-			}  else if (bl.getName().equals("Berlin")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Berlin.png");
-				BufferedImage berlin = faerbeLand(bwfarbe, landimg);
-				g.drawImage(berlin, 207, 108, this);
-			}  else if (bl.getName().equals("Brandenburg")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Brandenburg.png");
-				BufferedImage brandenburg = faerbeLand(bwfarbe, landimg);
-				g.drawImage(brandenburg, 167, 75, this);
-			}  else if (bl.getName().equals("Bremen")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Bremen.png");
-				BufferedImage bremen = faerbeLand(bwfarbe, landimg);
-				g.drawImage(bremen, 84, 70, this);
-			}  else if (bl.getName().equals("Hamburg")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Hamburg.png");
-				BufferedImage hamburg = faerbeLand(bwfarbe, landimg);
-				g.drawImage(hamburg, 119, 54, this);
-			}  else if (bl.getName().equals("Hessen")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Hessen.png");
-				BufferedImage hessen = faerbeLand(bwfarbe, landimg);
-				g.drawImage(hessen, 47, 147, this);
-			}  else if (bl.getName().equals("Mecklenburg-Vorpommern")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Mecklenburg-Vorpommern.png");
-				BufferedImage meckpom = faerbeLand(bwfarbe, landimg);
-				g.drawImage(meckpom, 145, 27, this);
-			}  else if (bl.getName().equals("Niedersachsen")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Niedersachsen.png");
-				BufferedImage niedersachsen = faerbeLand(bwfarbe, landimg);
-				g.drawImage(niedersachsen, 30, 45, this);
-			}  else if (bl.getName().equals("Nordrhein-Westfalen")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Nordrhein-Westfalen.png");
-				BufferedImage nrw = faerbeLand(bwfarbe, landimg);
-				g.drawImage(nrw, 0, 100, this);
-			}  else if (bl.getName().equals("Rheinland-Pfalz")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Rheinland-Pfalz.png");
-				BufferedImage rlp = faerbeLand(bwfarbe, landimg);
-				g.drawImage(rlp, 1, 170, this);
-			}  else if (bl.getName().equals("Saarland")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Saarland.png");
-				BufferedImage saarland = faerbeLand(bwfarbe, landimg);
-				g.drawImage(saarland, 5, 227, this);
-			}  else if (bl.getName().equals("Sachsen-Anhalt")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Sachsen-Anhalt.png");
-				BufferedImage sachsenAnhalt = faerbeLand(bwfarbe, landimg);
-				g.drawImage(sachsenAnhalt, 134, 89, this);
-			}  else if (bl.getName().equals("Sachsen")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Sachsen.png");
-				BufferedImage sachsen = faerbeLand(bwfarbe, landimg);
-				g.drawImage(sachsen, 169, 154, this);
-			}  else if (bl.getName().equals("Schleswig-Holstein")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Schleswig-Holstein.png");
-				BufferedImage schleswigHolstein = faerbeLand(bwfarbe, landimg);
-				g.drawImage(schleswigHolstein, 94, 0, this);
-			}  else if (bl.getName().equals("Thüringen")) {
-				Color bwfarbe = bl.getFarbe();
-				BufferedImage landimg = importImg("src\\gui\\resources\\Bundeslaender\\Thüringen.png");
-				BufferedImage thueringen = faerbeLand(bwfarbe, landimg);
-				g.drawImage(thueringen, 108, 150, this);
-			}
-			}
-			}
-		
-		private BufferedImage faerbeLand(Color bwfarbe, BufferedImage bild) {
-			/*int width = bild.getWidth();
-	        int height = bild.getHeight();
-
-	        for (int xx = 0; xx < width; xx++) {
-	            for (int yy = 0; yy < height; yy++) {
-	                Color originalColor = new Color(bild.getRGB(xx, yy), true);
-	                if (originalColor.equals(Color.WHITE) && originalColor.getAlpha() == 255) {
-	                    bild.setRGB(xx, yy, bwfarbe.getRGB());
-	                }
-	            }
-	        }*/
-	        return bild;
+		if (!bilderImportiert) {
+			importieren();
+			skaliere();
 		}
+			
+			faerbeLand();
+			erstelleGrafik();
+			g.drawImage(skaliert, 0, 0, this);
+		
+	}
 	
+	/**
+	 * ruft der Reihe nach die Bilder der Bundeslaender Deutschlands ab und legt sie in einem Array ab
+	 */
+	private void importieren() {
+		LinkedList<Bundesland> bundeslaender = land.getBundeslaender();
+		Collections.sort(bundeslaender);
+		for (int i = 0; i < 16; i++) {
+				bundeslandBilder[i] = importImg(pfad + bundeslaender.get(i).getName() + ".png");
+				bundeslandNamen[i] = bundeslaender.get(i).getName();
+		}
+		bilderImportiert = true;
+	}
 	
+	/**
+	 * erstellt die Deutschlandkarte indem es die eingefaerbten Bilder anhand der Koordinaten abbildet
+	 */
+	private void erstelleGrafik() {
+		Graphics2D grossVersionGraphics = grossVersion.createGraphics();
+		grossVersionGraphics.setBackground(Color.WHITE);
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[0], 175, 654, this);
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[1], 303, 552, this);
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[2], 620, 294, this);
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[3], 480, 182, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[4], 272, 226, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[5], 366, 165, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[6], 209, 428, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[7], 403, 34, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[8], 124, 94, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[9], 64, 319, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[10], 77, 506, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[11], 93, 664, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[12], 534, 418, this);
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[13], 421, 249, this);
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[14], 230, 0, this);			
+		grossVersionGraphics.drawImage(bundeslandBilderGefaerbt[15], 376, 426, this);
+	}
+
 	
-		
-		
-		
-		
-		
-		
-		private BufferedImage importImg(String pfad) {
+	/**
+	 * faerbt die Bilder der Bundeslaender entsprechend der Farbe der Partei die 
+	 * in diesem Bundesland den höchsten Zweitstimmenanteil hat. Das gefaerbte Bild wird in
+	 * dem Array bundeslandBilderGefaerbt abgelegt.
+	 */
+	private void faerbeLand() {
+		LinkedList<Bundesland> bundeslaender = land.getBundeslaender();
+		Collections.sort(bundeslaender);
+		for (int i = 0; i < 16; i++) {
+			BufferedImage aktuellesBild = bundeslandBilder[i];
+			Color bwfarbe = bundeslaender.get(i).getFarbe();
 			try {
-				BufferedImage landimg = ImageIO.read(new File(pfad));
-				return landimg;
+				int hoehe = aktuellesBild.getHeight();
+				int breite = aktuellesBild.getWidth();
+					for (int xx = 0; xx < breite; xx++) {
+						for (int yy = 0; yy < hoehe; yy++) {
+							Color originalColor = new Color(aktuellesBild.getRGB(xx, yy), true);
+							if (originalColor.equals(Color.WHITE) && originalColor.getAlpha() == 255) {
+								aktuellesBild.setRGB(xx, yy, bwfarbe.getRGB());
+							}
+						}
+					}
+			} catch (NullPointerException nPE) {
+				System.out.println("Keine Farbe vorhanden!");
+			}
+	    bundeslandBilderGefaerbt[i] = aktuellesBild;
+		}
+	}
+	
+	/**
+	 * importiert die am Uebergebenen Pfad vorgefundene Datei
+	 * @param pfad der Speicherort der zu importierenden Datei
+	 * @return die importierte Datei
+	 */
+	private BufferedImage importImg(String pfad) {
+			try {
+				BufferedImage landBild = ImageIO.read(new File(pfad));
+				return landBild;
 			} catch (IOException e) {
 				e.printStackTrace();
 				return null;
