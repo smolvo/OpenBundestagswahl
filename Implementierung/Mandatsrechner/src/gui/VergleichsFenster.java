b287e378f7232664c9f89541d7b66c4a38a58250
Anton Mehlmann <mehlton@web.de>
2014-01-22 12:31:59 +0000

    vergleichsfenster fertig

--- Implementierung/Mandatsrechner/src/gui/VergleichsFenster.java
+++ Implementierung/Mandatsrechner/src/gui/VergleichsFenster.java
@@ -1,10 +1,17 @@
 package gui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import gui.ansicht.diagrammfenster.BundDiagramm;
 import gui.ansicht.tabellenfenster.BundTableModel;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import wahlvergleich.Wahlvergleich;
 import wahlvergleich.WahlvergleichDaten;
 import wahlvergleich.WahlvergleichTableModel;
 
@@ -15,26 +22,55 @@ import wahlvergleich.WahlvergleichTableModel;
  * @author Anton
  * 
  */
-public class VergleichsFenster extends JScrollPane {
+public class VergleichsFenster extends JFrame {
+
+	/** repräsentiert das Layout */
+	private GridBagConstraints gbc;
 
 	/**
 	 * Der Konstruktor erstellt ein Vergleichsfenster.
 	 * 
-	 * @param daten
+	 * @param vergleich
+	 *            Vergleichsdaten
 	 */
-	public VergleichsFenster(WahlvergleichDaten daten) {
-		zeigeVergleich(daten);
+	public VergleichsFenster(Wahlvergleich vergleich) {
+		zeigeVergleich(vergleich);
 	}
 
 	/**
 	 * Diese Methode füllt das Fenster mit der Tabelle.
 	 * 
-	 * @param daten
-	 *            Daten
+	 * @param vergleich
+	 *            der Vergleich
 	 */
-	private void zeigeVergleich(WahlvergleichDaten daten) {
+	private void zeigeVergleich(Wahlvergleich vergleich) {
+		WahlvergleichDaten daten = vergleich.wahlvergleich();
 		WahlvergleichTableModel tabelle = new WahlvergleichTableModel(daten);
 		JTable jTabelle = new JTable(tabelle);
-		this.setViewportView(jTabelle);
+		JScrollPane scrollPane = new JScrollPane(jTabelle);
+
+		this.gbc = new GridBagConstraints();
+		gbc.weightx = 0.5;
+		gbc.weighty = 0.5;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 2;
+		gbc.fill = GridBagConstraints.BOTH;
+		add(scrollPane, gbc);
+
+		BundDiagramm diagrammErsteWahl = new BundDiagramm(vergleich.getBtw1(),
+				new JPanel());
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+		gbc.fill = GridBagConstraints.BOTH;
+		add(diagrammErsteWahl, gbc);
+
+		BundDiagramm diagrammZweiteWahl = new BundDiagramm(vergleich.getBtw2(),
+				new JPanel());
+		gbc.gridx = 2;
+		gbc.gridy = 1;
+		gbc.fill = GridBagConstraints.BOTH;
+		add(diagrammZweiteWahl, gbc);
+
 	}
 }
