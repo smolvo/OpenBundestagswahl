aaaeecab04b60718d2826186a3ce3d70201cf885
Simon Schürg <simon.schuerg@gmail.com>
2014-01-28 14:57:53 +0000

    GridBagLayout ist echt übel...

--- Implementierung/Mandatsrechner/src/main/java/gui/dialoge/HandbuchDialog.java
+++ Implementierung/Mandatsrechner/src/main/java/gui/dialoge/HandbuchDialog.java
@@ -1,6 +1,9 @@
 package main.java.gui.dialoge;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.io.File;
 import java.net.MalformedURLException;
 
@@ -12,6 +15,7 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.WindowConstants;
 
 /**
@@ -44,18 +48,38 @@ public class HandbuchDialog extends JDialog {
 		
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		
-		GridLayout layout = new GridLayout(1, 2);
-		layout.setHgap(3);
-		layout.setVgap(3);
+		//GridLayout layout = new GridLayout(1, 4);
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints gbc=new GridBagConstraints();
+		// Festlegen, dass die GUI-Elemente die Gitterfelder in waagerechter Richtung ausfüllen:
+		gbc.fill=GridBagConstraints.VERTICAL;
+		// Als Layout festlegen
 		setLayout(layout);
 		
+		/*
+		 * Erste Spalte (index 0)
+		 */
+		JLabel label = new JLabel("asdasd");
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridwidth = 1;
+		layout.setConstraints(label, gbc);
+		this.add(label, gbc);
+		
+		/*
+		 * Zweite (index 1) bis vierte (index 3) Spalte 
+		 */
 		final JFXPanel fxPanel = new JFXPanel();
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+		gbc.gridwidth = 3;
+		layout.setConstraints(fxPanel, gbc);
+		this.add(fxPanel, gbc);
+		
 		//JScrollPane scrollPane = new JScrollPane();
         //scrollPane.add(fxPanel);
-        
         //fxPanel.add(scrollPane);
-        
-		this.add(fxPanel);
+		
 		
 		Platform.runLater(new Runnable() {
 			// fxPanel als JavaFX-Thread starten
