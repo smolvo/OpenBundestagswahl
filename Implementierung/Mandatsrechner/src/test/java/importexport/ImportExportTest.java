a6113181fc6af772e6f8da99a89ded549f476777
Enes Ördek <genesis@proxer.de>
2014-02-26 12:45:22 +0000

    Erste zwei Tests für Import/Export.

--- Implementierung/Mandatsrechner/src/test/java/importexport/ImportExportTest.java
+++ Implementierung/Mandatsrechner/src/test/java/importexport/ImportExportTest.java
@@ -1,6 +1,15 @@
 package test.java.importexport;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.io.File;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import main.java.importexport.ImportExportManager;
 import main.java.model.Bundestagswahl;
@@ -10,68 +19,117 @@ import main.java.model.Bundestagswahl;
  */
 public class ImportExportTest {
 
+	private static ImportExportManager i;
+	private static String[] filePaths;
+	
 	/**
-	 * @param args
-	 *            Argumente
+	 * @throws java.lang.Exception
 	 */
-	public static void main(String[] args) {
-
-		ImportExportManager i = new ImportExportManager();
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		 i = new ImportExportManager();
+		 filePaths = new String[5];
+		 filePaths[0] = "src/main/resources/importexport/Ergebnis2013.csv";
+		 filePaths[1] = "src/main/resources/importexport/Wahlbewerber2013.csv";
+		 filePaths[2] = "src/main/resources/importexport/Exported.csv";
+		 filePaths[3] = "src/main/resources/importexport/Exported2.csv";
+		 filePaths[4] = "src/main/resources/importexport/random.csv";
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+		
+	}
+	
+	/**
+	 * Basistest. Testet das Import/Export Modul auf 
+	 * Funktionalitaet.
+	 */
+	@Test
+	public void basisTest() {
 		File[] csvDateien = new File[2];
-		csvDateien[0] = new File("src/main/resources/importexport/Exported.csv");
-		csvDateien[1] = new File(
-				"src/main/resources/importexport/Wahlbewerber2013.csv");
+		csvDateien[0] = new File(filePaths[0]);
+		csvDateien[1] = new File(filePaths[1]);
+		
 		Bundestagswahl w = null;
+
 		try {
 			w = i.importieren(csvDateien);
-		} catch (Exception e1) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			System.out.println("Leine gï¿½ltige CSV-Datei :/");
+			e.printStackTrace();
 		}
+		
+		assertNotNull(w);
+		
+		boolean result = i.exportieren(filePaths[2], w);
+		
+		assertTrue(result);
+	}
+	
+	/**
+	 * Importiert eine Datei, Exportiert sie,
+	 * importiert sie dann erneut und exportiert
+	 * sie dann nochmal.
+	 */
+	@Test
+	public void doubleImportExportTest() {
+		File[] csvDateien = new File[2];
+		csvDateien[0] = new File(filePaths[0]);
+		csvDateien[1] = new File(filePaths[1]);
+		
+		Bundestagswahl w = null;
 
-		i.exportieren("src/main/resources/importexport/Exported2.csv", w);
+		try {
+			w = i.importieren(csvDateien);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertNotNull(w);
+		
+		boolean result = i.exportieren(filePaths[2], w);
+		
+		assertTrue(result);
+		
+		csvDateien[0] = new File(filePaths[2]);
+		csvDateien[1] = new File(filePaths[1]);
+		
+		
+		w = null;
 
-		// System.out.println(csvDatei.getName());
-		// int integer = Integer.parseInt("");
-
-		// System.out.println(w.getDeutschland().getWahlkreise().get(0).getAnzahlZweitstimmen());
-		// System.out.println(w.getDeutschland().getWahlkreise().get(0).hashCode());
-
-		// clone durch Serialisierung
-		/*
-		 * System.out.println("\n\n------------  clone  ------------"); try { //
-		 * clone Bundestagswahl w2 = w.deepCopy();
-		 * 
-		 * System.out.println(w2.getDeutschland().getWahlkreise().get(0).
-		 * getAnzahlZweitstimmen());
-		 * System.out.println(w2.getDeutschland().getWahlkreise
-		 * ().get(0).hashCode());
-		 * 
-		 * // ein paar kleine Tests
-		 * System.out.println(w.getDeutschland().getBundeslaender
-		 * ().getFirst().getName());
-		 * System.out.println(w2.getDeutschland().getBundeslaender
-		 * ().getFirst().getName());
-		 * 
-		 * w2.getDeutschland().getBundeslaender().getFirst().setName("BLABLA");
-		 * 
-		 * System.out.println(w.getDeutschland().getBundeslaender().getFirst().
-		 * getName());
-		 * System.out.println(w2.getDeutschland().getBundeslaender().
-		 * getFirst().getName());
-		 * 
-		 * w2.getDeutschland().getBundeslaender().set(0,
-		 * w2.getDeutschland().getBundeslaender().getLast());
-		 * 
-		 * System.out.println(w.getDeutschland().getBundeslaender().getFirst().
-		 * getName());
-		 * System.out.println(w2.getDeutschland().getBundeslaender().
-		 * getFirst().getName());
-		 * 
-		 * } catch (Exception e) { e.printStackTrace(); }
-		 */
-
+		try {
+			w = i.importieren(csvDateien);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertNotNull(w);
+		
+		result = i.exportieren(filePaths[3], w);
+		
+		assertTrue(result);
+		
 	}
 
 }
