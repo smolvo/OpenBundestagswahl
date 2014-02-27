60edefa3c7f9aee7d0c476ac5f992beae0af3dc9
Nick Vlasoff <nickvlasoff@googlemail.com>
2014-02-27 10:59:44 +0000

    Weitere Tests für den Mandatsrechner

--- Implementierung/Mandatsrechner/src/test/java/mandatsrechner/Mandatsrechner2009Test.java
+++ Implementierung/Mandatsrechner/src/test/java/mandatsrechner/Mandatsrechner2009Test.java
@@ -25,17 +25,17 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 /**
- * @author Nickes
+ * 
  *
  */
 public class Mandatsrechner2009Test {
 
 	private Mandatsrechner2009 rechner;
-	
+
 	private static Bundestagswahl wahl;
-	
+
 	private Bundestagswahl cloneWahl;
-	
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		// Wahl 2013
@@ -46,18 +46,18 @@ public class Mandatsrechner2009Test {
 				"src/main/resources/importexport/Wahlbewerber2013.csv");
 		wahl = Steuerung.getInstance().importieren(csvDateien);
 	}
-	
+
 	@Before
 	public void setUp() throws Exception {
 		this.cloneWahl = wahl.deepCopy();
 		this.rechner = Mandatsrechner2009.getInstance();
 	}
-	
+
 	@After
 	public void tearDown() throws Exception {
 		this.cloneWahl = null;
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void initialisiereTest1() {
 		rechner.initialisiere(null);
@@ -65,15 +65,19 @@ public class Mandatsrechner2009Test {
 
 	@Test
 	public void initialisiereTest2() {
-		LinkedList<Kandidat> mitgliederPartei1 = this.cloneWahl.getParteien().get(3).getMitglieder();
-		LinkedList<Kandidat> mitgliederPartei2 = this.cloneWahl.getParteien().get(0).getMitglieder();
+		LinkedList<Kandidat> mitgliederPartei1 = this.cloneWahl.getParteien()
+				.get(3).getMitglieder();
+		LinkedList<Kandidat> mitgliederPartei2 = this.cloneWahl.getParteien()
+				.get(0).getMitglieder();
+		rechner.berechneSainteLague(this.cloneWahl);
 		rechner.initialisiere(this.cloneWahl);
-		LinkedList<Kandidat> kandidaten = this.cloneWahl.getSitzverteilung().getAbgeordnete();
+		LinkedList<Kandidat> kandidaten = this.cloneWahl.getSitzverteilung()
+				.getAbgeordnete();
 		BerichtDaten bericht = this.cloneWahl.getSitzverteilung().getBericht();
-		
+
 		assertEquals(0, kandidaten.size());
 		assertEquals(0, bericht.getBundeslaender().size());
-		
+
 		for (Kandidat kan : mitgliederPartei1) {
 			assertEquals(Mandat.KEINMANDAT, kan.getMandat());
 		}
@@ -81,39 +85,41 @@ public class Mandatsrechner2009Test {
 			assertEquals(Mandat.KEINMANDAT, kan.getMandat());
 		}
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void berechneDirektmandatTest1() {
 		this.rechner.testBerechneDirektmandat(null);
 	}
-	
+
 	@Test
 	public void berechneDirektmandatTest2() {
-		Wahlkreis wk = this.cloneWahl.getDeutschland().getBundeslaender().get(0).getWahlkreise().get(0);
+		Wahlkreis wk = this.cloneWahl.getDeutschland().getBundeslaender()
+				.get(0).getWahlkreise().get(0);
 		Kandidat gewinner = wk.getErststimmenProPartei().get(0).getKandidat();
 		wk.getErststimmenProPartei().get(0).setAnzahl(Integer.MAX_VALUE);
-		
+
 		this.rechner.initialisiere(this.cloneWahl);
-		
+
 		this.rechner.testBerechneDirektmandat(this.cloneWahl);
-		
+
 		assertEquals(Mandat.DIREKTMANDAT, gewinner.getMandat());
 	}
-	
+
 	@Test
 	public void berechneDirektmandatTest3() {
-		Wahlkreis wk = this.cloneWahl.getDeutschland().getBundeslaender().get(0).getWahlkreise().get(0);
+		Wahlkreis wk = this.cloneWahl.getDeutschland().getBundeslaender()
+				.get(0).getWahlkreise().get(0);
 		Kandidat gewinner1 = wk.getErststimmenProPartei().get(0).getKandidat();
 		Kandidat gewinner2 = wk.getErststimmenProPartei().get(1).getKandidat();
 		Kandidat gewinner3 = wk.getErststimmenProPartei().get(2).getKandidat();
 		wk.getErststimmenProPartei().get(0).setAnzahl(Integer.MAX_VALUE);
 		wk.getErststimmenProPartei().get(1).setAnzahl(Integer.MAX_VALUE);
 		wk.getErststimmenProPartei().get(2).setAnzahl(Integer.MAX_VALUE);
-		
+
 		this.rechner.initialisiere(this.cloneWahl);
-		
+
 		this.rechner.testBerechneDirektmandat(this.cloneWahl);
-		
+
 		if (gewinner1.getMandat().equals(Mandat.DIREKTMANDAT)) {
 			assertEquals(Mandat.KEINMANDAT, gewinner2.getMandat());
 			assertEquals(Mandat.KEINMANDAT, gewinner3.getMandat());
@@ -126,35 +132,44 @@ public class Mandatsrechner2009Test {
 			assertEquals(Mandat.DIREKTMANDAT, gewinner3.getMandat());
 		}
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void berechneRelevanteParteienTest1() {
 		this.rechner.testBerechneRelevanteParteien(null);
 	}
-	
+
 	@Test
 	public void berechneRelevanteParteienTest2() {
-		Bundesland sa = this.cloneWahl.getDeutschland().getBundeslaender().get(14);
-		Bundesland rlp = this.cloneWahl.getDeutschland().getBundeslaender().get(6);
-		
+		Bundesland sa = this.cloneWahl.getDeutschland().getBundeslaender()
+				.get(14);
+		Bundesland rlp = this.cloneWahl.getDeutschland().getBundeslaender()
+				.get(6);
+
 		Partei cdu = this.cloneWahl.getParteien().get(0);
-		
+
 		Partei tierschutz = this.cloneWahl.getParteien().get(8);
 		// TierschutzPartei in Harz und Magdeburg gewinnen lassen
-		sa.getWahlkreise().get(2).getErststimmenProPartei().get(8).setAnzahl(Integer.MAX_VALUE);
-		sa.getWahlkreise().get(3).getErststimmenProPartei().get(8).setAnzahl(Integer.MAX_VALUE);
-		
+		sa.getWahlkreise().get(2).getErststimmenProPartei().get(8)
+				.setAnzahl(Integer.MAX_VALUE);
+		sa.getWahlkreise().get(3).getErststimmenProPartei().get(8)
+				.setAnzahl(Integer.MAX_VALUE);
+
 		Partei rep = this.cloneWahl.getParteien().get(9);
-		// Rep in Ludwigshafen/Frankenthal, Pirmasens und in der Südpfalz gewinnen lassen
-		rlp.getWahlkreise().get(10).getErststimmenProPartei().get(9).setAnzahl(Integer.MAX_VALUE);
-		rlp.getWahlkreise().get(13).getErststimmenProPartei().get(9).setAnzahl(Integer.MAX_VALUE);
-		rlp.getWahlkreise().get(14).getErststimmenProPartei().get(9).setAnzahl(Integer.MAX_VALUE);
-		
+		// Rep in Ludwigshafen/Frankenthal, Pirmasens und in der Südpfalz
+		// gewinnen lassen
+		rlp.getWahlkreise().get(10).getErststimmenProPartei().get(9)
+				.setAnzahl(Integer.MAX_VALUE);
+		rlp.getWahlkreise().get(13).getErststimmenProPartei().get(9)
+				.setAnzahl(Integer.MAX_VALUE);
+		rlp.getWahlkreise().get(14).getErststimmenProPartei().get(9)
+				.setAnzahl(Integer.MAX_VALUE);
+
 		this.rechner.initialisiere(this.cloneWahl);
 		this.rechner.testBerechneDirektmandat(this.cloneWahl);
-		
-		LinkedList<Partei> relevante = this.rechner.testBerechneRelevanteParteien(this.cloneWahl);
-		
+
+		LinkedList<Partei> relevante = this.rechner
+				.testBerechneRelevanteParteien(this.cloneWahl);
+
 		boolean enthaeltCDU = false;
 		boolean enthaeltTS = false;
 		boolean enthaeltREP = false;
@@ -167,21 +182,26 @@ public class Mandatsrechner2009Test {
 				enthaeltREP = true;
 			}
 		}
-		assertEquals(tierschutz, sa.getWahlkreise().get(2).getWahlkreisSieger().getPartei());
-		assertEquals(tierschutz, sa.getWahlkreise().get(3).getWahlkreisSieger().getPartei());
-		assertEquals(rep, rlp.getWahlkreise().get(10).getWahlkreisSieger().getPartei());
-		assertEquals(rep, rlp.getWahlkreise().get(13).getWahlkreisSieger().getPartei());
-		assertEquals(rep, rlp.getWahlkreise().get(14).getWahlkreisSieger().getPartei());
+		assertEquals(tierschutz, sa.getWahlkreise().get(2).getWahlkreisSieger()
+				.getPartei());
+		assertEquals(tierschutz, sa.getWahlkreise().get(3).getWahlkreisSieger()
+				.getPartei());
+		assertEquals(rep, rlp.getWahlkreise().get(10).getWahlkreisSieger()
+				.getPartei());
+		assertEquals(rep, rlp.getWahlkreise().get(13).getWahlkreisSieger()
+				.getPartei());
+		assertEquals(rep, rlp.getWahlkreise().get(14).getWahlkreisSieger()
+				.getPartei());
 		assertTrue(enthaeltCDU);
 		assertFalse(enthaeltTS);
 		assertTrue(enthaeltREP);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void rundenTest1() {
 		this.rechner.testRunden(-1, false);
 	}
-	
+
 	@Test(timeout = 10)
 	public void rundenTest2() {
 		assertEquals(0, this.rechner.testRunden((float) 0.4, false));
@@ -196,17 +216,63 @@ public class Mandatsrechner2009Test {
 			}
 		}
 	}
-	
+
 	@Test
 	public void getRelevanteZweitstimmenSummeTest() {
-		LinkedList<Partei> parteien1 = this.cloneWahl.getDeutschland().getBundeslaender().get(0).getParteien();
-		LinkedList<Partei> parteien2 = this.cloneWahl.getDeutschland().getBundeslaender().get(2).getParteien();
-		Bundesland land1 = this.cloneWahl.getDeutschland().getBundeslaender().get(0);
-		Bundesland land2 = this.cloneWahl.getDeutschland().getBundeslaender().get(1);
+		LinkedList<Partei> parteien1 = this.cloneWahl.getDeutschland()
+				.getBundeslaender().get(0).getParteien();
+		LinkedList<Partei> parteien2 = this.cloneWahl.getDeutschland()
+				.getBundeslaender().get(2).getParteien();
+		Bundesland land1 = this.cloneWahl.getDeutschland().getBundeslaender()
+				.get(0);
+		Bundesland land2 = this.cloneWahl.getDeutschland().getBundeslaender()
+				.get(1);
+
+		assertEquals(1628290,
+				this.rechner.getRelevanteZweitstimmenSumme(parteien1, land1));
+		assertEquals(883223,
+				this.rechner.getRelevanteZweitstimmenSumme(parteien1, land2));
+		assertEquals(1621447,
+				this.rechner.getRelevanteZweitstimmenSumme(parteien2, land1));
+		assertEquals(879404,
+				this.rechner.getRelevanteZweitstimmenSumme(parteien2, land2));
+	}
+
+	@Test
+	public void berechneSainteLagueTest() {
+		int spd = 0, cdu = 0, csu = 0, gruene = 0, linke = 0, sonst = 0;
+
+		this.rechner.berechneSainteLague(this.cloneWahl);
+
+		for (Kandidat kandidat : this.cloneWahl.getSitzverteilung()
+				.getAbgeordnete()) {
+			switch (kandidat.getPartei().getName()) {
+			case "CDU":
+				cdu++;
+				break;
+			case "SPD":
+				spd++;
+				break;
+			case "CSU":
+				csu++;
+				break;
+			case "GRÜNE":
+				gruene++;
+				break;
+			case "DIE LINKE":
+				linke++;
+				break;
+			default:
+				sonst++;
+				break;
+			}
+		}
+
 		
-		assertEquals(1628290, this.rechner.getRelevanteZweitstimmenSumme(parteien1, land1));
-		assertEquals(883223, this.rechner.getRelevanteZweitstimmenSumme(parteien1, land2));
-		assertEquals(1621447, this.rechner.getRelevanteZweitstimmenSumme(parteien2, land1));
-		assertEquals(879404, this.rechner.getRelevanteZweitstimmenSumme(parteien2, land2));
+		assertEquals(182, spd);
+		assertEquals(60, gruene);
+		assertEquals(61, linke);
+		assertEquals(53, csu);
+		assertEquals(246, cdu);
 	}
 }
--- Implementierung/Mandatsrechner/src/test/java/mandatsrechner/Mandatsrechner2013Test.java
+++ Implementierung/Mandatsrechner/src/test/java/mandatsrechner/Mandatsrechner2013Test.java
@@ -5,49 +5,113 @@ package test.java.mandatsrechner;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.util.LinkedList;
+
+import main.java.mandatsrechner.Mandatsrechner2009;
+import main.java.mandatsrechner.Mandatsrechner2013;
+import main.java.model.BerichtDaten;
+import main.java.model.Bundesland;
+import main.java.model.Bundestagswahl;
+import main.java.model.Kandidat;
+import main.java.model.Mandat;
+import main.java.model.Partei;
+import main.java.model.Wahlkreis;
+import main.java.steuerung.Steuerung;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+
+
+
+
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import test.java.Debug;
+
 /**
- * @author Nickes
+ * 
  *
  */
 public class Mandatsrechner2013Test {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
+	private Mandatsrechner2013 rechner;
+
+	private static Bundestagswahl wahl;
+
+	private Bundestagswahl cloneWahl;
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		// Wahl 2013
+		File[] csvDateien = new File[2];
+		csvDateien[0] = new File(
+				"src/main/resources/importexport/Ergebnis2013.csv");
+		csvDateien[1] = new File(
+				"src/main/resources/importexport/Wahlbewerber2013.csv");
+		wahl = Steuerung.getInstance().importieren(csvDateien);
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@Before
 	public void setUp() throws Exception {
+		this.cloneWahl = wahl.deepCopy();
+		this.rechner = Mandatsrechner2013.getInstance();
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@After
 	public void tearDown() throws Exception {
+		this.cloneWahl = null;
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test(expected = IllegalArgumentException.class)
+	public void initialisiereTest1() {
+		int spd = 0, cdu = 0, csu = 0, gruene = 0, linke = 0, sonst = 0;
+		this.rechner.berechne(this.cloneWahl);
+		for (int i = 0; i < this.cloneWahl.getSitzverteilung().getAbgeordnete().size(); i++) {
+			switch (this.cloneWahl.getSitzverteilung().getAbgeordnete().get(i).getPartei().getName()) {
+			case "CDU":
+				cdu++;
+				break;
+			case "SPD":
+				spd++;
+				break;
+			case "CSU":
+				csu++;
+				break;
+			case "GRÜNE":
+				gruene++;
+				break;
+			case "DIE LINKE":
+				linke++;
+				break;
+			default:
+				sonst++;
+				break;
+			}
+		}
+
+		assertNotEquals(0, spd);
+		assertNotEquals(0, gruene);
+		assertNotEquals(0, linke);
+		assertNotEquals(0, csu);
+		assertNotEquals(0, cdu);
+		
+		assertEquals(193, spd);
+		assertEquals(63, gruene);
+		assertEquals(64, linke);
+		assertEquals(56, csu);
+		assertEquals(255, cdu);
 	}
 
 }
