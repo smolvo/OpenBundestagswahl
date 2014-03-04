08f499d2a5b0b5c6959236423bc4731676a0a805
Philipp Löwer <ph.loewer@hotmail.de>
2014-03-04 18:21:17 +0000

    JavaFX entfernt

--- Implementierung/Mandatsrechner/src/main/java/gui/dialoge/AboutDialog.java
+++ Implementierung/Mandatsrechner/src/main/java/gui/dialoge/AboutDialog.java
@@ -1,92 +1,79 @@
 package main.java.gui.dialoge;
 
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.io.File;
 import java.net.MalformedURLException;
 
 import javax.swing.JDialog;
+import javax.swing.JEditorPane;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLEditorKit;
 
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 
-/**
- * Diese Klasse repräsentiert die "About" View in der GUI. Es werden
- * Informationen zu diesem Programm und den Entwicklern angezeigt. Es wird eine
- * Webview erzeugt die, die genannten Informationen anzeigt.
- */
-public class AboutDialog extends JDialog {
 
-	/** Automatisch generierte serialVersionUID */
-	private static final long serialVersionUID = -1091530997927819189L;
 
-	/** Die HTML-Datei die in der Webview angezeigt wird. */
-	private static final File ABOUT_FILE = new File(
-			"src/main/resources/hilfe/about.html");
-
-	/**
-	 * Das Diallogfenster, dass die Webview enthï¿½lt
-	 */
+public class AboutDialog extends JDialog{
+	
+	private static final String ABOUT_FILE = 
+		"<html>\n"
+				+ "<head>\n"
+				+ "<title>About</title>\n"
+				+ "<link type=\"text/css\" href=\"style.css\" rel=\"stylesheet\"/>\n"
+						+ "</head>\n"
+						+ "<body>\n"
+						+ "<div id=\"container\">\n"
+								+ "<p align=\"center\">\n"
+										+ "<img src=\"KIT-Logo.png\" height=\"100\"/>\n"
+												+ "</p>\n"
+												+ "<h1>Bundestagswahl</h1>\n"
+												+ "	Dieses Programm wurde im Rahmen eines Projektes des KIT (Karlsruhe Institute of Technologie) \n"
+												+ "erstellt und soll die Sitzverteilung des deutschen Bundestages gem&auml;&szlig; der \n"
+												+ "gesetzlichen Bestimmungen exakt berechnen.\n"
+		
++ "<h3>Entwickler</h3>\n"
++ "Philipp L&ouml;wer <a href=\"\">Mail-Adresse</a><br/>\n"
++ "Simon Sch&uuml;rg <a href=\"mailto:simon.schuerg@gmail.com\">simon.schuerg@gmail.com</a><br/>\n"
+		+ "Manuel Olk <a href=\"\">Mail-Adresse</a><br/>\n"
+		+ "Nick Vlassoff <a href=\"\">Mail-Adresse</a><br/>\n"
+		+ "Anton Mehlmann <a href=\"\">Mail-Adresse</a><br/>\n"
+		+ "Enes &Ouml;rdek <a href=\"mailto:13genesis37@gmail.com\">13genesis37@gmail.com</a>\n"
+		
++ "<h3>Verwendete Bibliotheken</h3>\n"
++ "Die folgenden verwendeten Bibliotheken sind unter der <b>GNU Lesser General Public License</b> lizensiert.\n"
++ "<ul>\n"
++ "<li>JFreeChart</li>\n"
++ "<li>JCommon</li>\n"
++ "<ul>\n"
+		
++ "</div>\n"
++ "</body>\n"
++ "</html>\n";
+			
+	
 	public AboutDialog() {
-
-		// allgemeine Anpassungen des Fensters
-		setTitle("Über diese Anwendung");
-		setSize(600, 600);
-		setLocationRelativeTo(null);
-		setResizable(true);
-		setAlwaysOnTop(true);
-
-		// wird benï¿½tigt um JavaFX Panel zu DISPOSEN
-		Platform.setImplicitExit(false);
-		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		GridLayout layout = new GridLayout(1, 1);
-
-		// Als Layout festlegen
-		setLayout(layout);
-		final JFXPanel fxPanel = new JFXPanel();
-		this.add(fxPanel);
-
-		Platform.runLater(new Runnable() {
-			// fxPanel als JavaFX-Thread starten
-			@Override
-			public void run() {
-				initFX(fxPanel);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run () {
+				JEditorPane jEditorPane = new JEditorPane();
+				jEditorPane.setEditable(false);
+				
+				JScrollPane scrollPane = new JScrollPane(jEditorPane);
+				
+				HTMLEditorKit kit = new HTMLEditorKit();
+				jEditorPane.setEditorKit(kit);
+				Document doc = kit.createDefaultDocument();
+				jEditorPane.setDocument(doc);
+				
+				jEditorPane.setText(ABOUT_FILE);
+				add(scrollPane, BorderLayout.CENTER);
+				setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+				setSize(new Dimension(600, 600));
+				pack();
+				setVisible(true);
 			}
 		});
-
-		setVisible(true);
-	}
-
-	/**
-	 * Erzeugt eine Webview und läd about.html
-	 * 
-	 * @param fxPanel
-	 *            das Panel in dem die Webview gestartet wird
-	 */
-	private static void initFX(final JFXPanel fxPanel) {
-		Scene scene = createScene();
-		fxPanel.setScene(scene);
-
-	}
-
-	private static Scene createScene() {
-		Group group = new Group();
-		Scene scene = new Scene(group);
-		WebView webView = new WebView();
-		webView.setMaxWidth(585);
-		webView.setMaxHeight(565);
-		group.getChildren().add(webView);
-		WebEngine webEngine = webView.getEngine();
-
-		try {
-			webEngine.load(ABOUT_FILE.toURI().toURL().toString());
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		return (scene);
 	}
 }
