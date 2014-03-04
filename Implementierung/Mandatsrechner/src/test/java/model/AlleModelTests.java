d884cc284000a6ed6b25c79244c2fc8d06c06bb6
Manuel Olk <manuel.olk@gmx.de>
2014-03-04 12:50:45 +0000

    Test-Suite angepasst

--- Implementierung/Mandatsrechner/src/test/java/model/AlleModelTests.java
+++ Implementierung/Mandatsrechner/src/test/java/model/AlleModelTests.java
@@ -5,9 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ BundeslandTest.class, DeutschlandTest.class,
-		LandeslisteTest.class, ParteiTest.class, TestErststimme.class,
-		WahlkreisTest.class, ZweitstimmeTest.class })
+@SuiteClasses({ BundeslandTest.class, DeutschlandTest.class, ErststimmeTest.class, GebietTest.class, KandidatTest.class,
+		LandeslisteTest.class, ParteiTest.class, WahlkreisTest.class, ZweitstimmeTest.class })
 public class AlleModelTests {
 
 }
