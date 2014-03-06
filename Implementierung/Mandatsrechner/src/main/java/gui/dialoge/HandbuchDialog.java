9342f296cd3fcfde81f9945ebe9f673d9f93afbc
Philipp Löwer <ph.loewer@hotmail.de>
2014-03-06 11:45:04 +0000

    groesse handbuchfenster geaendert (refs #57)

--- Implementierung/Mandatsrechner/src/main/java/gui/dialoge/HandbuchDialog.java
+++ Implementierung/Mandatsrechner/src/main/java/gui/dialoge/HandbuchDialog.java
@@ -3,6 +3,7 @@ package main.java.gui.dialoge;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Frame;
 import java.io.File;
 import java.io.FileReader;
 
@@ -30,13 +31,14 @@ public class HandbuchDialog {
 					editor.read(fr, "HTML");
 					
 					JFrame frame = new JFrame ("How to use");
-					frame.setPreferredSize(new Dimension(500, 500));
+					frame.setExtendedState(Frame.MAXIMIZED_BOTH);
 					frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 					frame.setLayout(new BorderLayout());
 					frame.add(new JScrollPane(editor));
 					frame.pack();
 					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
+					
 					
 				} catch (Exception e) {
 					e.printStackTrace();
